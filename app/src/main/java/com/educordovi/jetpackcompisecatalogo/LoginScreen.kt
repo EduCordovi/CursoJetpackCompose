e1bb152422cf0c3ad0144a7145b8d3b1918ee78b
EduCordovi <educordovi@gmail.com>
2023-12-28 13:17:17 -0500

    Se personaliza botón de login

--- app/src/main/java/com/educordovi/jetpackcompisecatalogo/LoginScreen.kt
+++ app/src/main/java/com/educordovi/jetpackcompisecatalogo/LoginScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -165,7 +166,18 @@ fun LoginDivider() {
 
 @Composable
 fun LoginButton(loginEnable: Boolean) {
-    Button(onClick = { }, enabled = loginEnable, modifier = Modifier.fillMaxWidth()) {
+    Button(
+        onClick = { },
+        enabled = loginEnable,
+        modifier = Modifier.fillMaxWidth(),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color(0XFF4EA8E9),
+            disabledContainerColor = Color(0XFF78C8F9),
+            contentColor = Color.White,
+            disabledContentColor = Color.White
+
+        )
+    ) {
         Text(text = "Log In")
     }
 }
