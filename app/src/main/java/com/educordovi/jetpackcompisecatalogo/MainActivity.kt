16adb53fe0c8b5c11cf2bd08ef8d9591a68cd36c
EduCordovi <educordovi@gmail.com>
2024-02-05 12:14:46 -0500

    Se crea ejemplo de Navegacion Basica Jetpack Compose

--- app/src/main/java/com/educordovi/jetpackcompisecatalogo/MainActivity.kt
+++ app/src/main/java/com/educordovi/jetpackcompisecatalogo/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.educordovi.jetpackcompisecatalogo
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
@@ -11,7 +9,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -23,9 +20,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
@@ -37,7 +31,6 @@ import androidx.compose.material3.BadgedBox
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CardDefaults.cardElevation
 import androidx.compose.material3.Checkbox
@@ -54,7 +47,6 @@ import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.RadioButtonDefaults
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
 import androidx.compose.material3.SwitchDefaults
@@ -73,7 +65,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.state.ToggleableState
 import androidx.compose.ui.text.TextStyle
@@ -83,6 +74,9 @@ import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.educordovi.jetpackcompisecatalogo.ui.CheckInfo
 import com.educordovi.jetpackcompisecatalogo.ui.theme.JetpackCompiseCatalogoTheme
 
@@ -153,7 +147,21 @@ class MainActivity : ComponentActivity() {
                         RetoTwit(darkTheme)
                      */
 
-                    MyScaffoldTopBar()
+
+                    /*Parte implementada para el Navigation.kt
+                    link a usar para ejemplos de navegacion externa con intent podria cambiar en el futuro
+                    * https://stackoverflow.com/questions/66940091/how-do-i-start-an-intent-inside-a-compose-functions-button-click
+                    * */
+                    val navigationController = rememberNavController()
+                    NavHost(navController = navigationController, startDestination = "pantalla1" ){
+                        composable("pantalla1"){ Screen1(navigationController) }
+                        composable("pantalla2"){ Screen2(navigationController) }
+                        composable("pantalla3"){ Screen3(navigationController) }
+                    }
+
+
+
+                    //MyScaffoldTopBar()
 
                }
            }
---
+++ app/src/main/java/com/educordovi/jetpackcompisecatalogo/Navigation.kt
@@ -0,0 +1,45 @@
+package com.educordovi.jetpackcompisecatalogo
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavHostController
+
+@Composable
+fun Screen1(navController: NavHostController) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Cyan)
+    ) {
+        Text(text = "Pantalla 1", modifier = Modifier.align(Alignment.Center).clickable { navController.navigate("pantalla2") })
+    }
+}
+
+@Composable
+fun Screen2(navController: NavHostController) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Green)
+    ) {
+        Text(text = "Pantalla 2", modifier = Modifier.align(Alignment.Center).clickable { navController.navigate("pantalla3") })
+    }
+}
+
+@Composable
+fun Screen3(navController: NavHostController) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Magenta)
+    ) {
+        Text(text = "Pantalla 3", modifier = Modifier.align(Alignment.Center))
+    }
+}
