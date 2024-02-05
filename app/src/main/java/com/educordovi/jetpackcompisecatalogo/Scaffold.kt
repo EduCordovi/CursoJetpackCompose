52b086ce74ef6959158dcbfdc761d28e32eccf99
EduCordovi <educordovi@gmail.com>
2024-02-05 11:45:08 -0500

    Se crea ejemplo de ModalNavigationDrawer Jetpack Compose Material 3

--- app/src/main/java/com/educordovi/jetpackcompisecatalogo/Scaffold.kt
+++ app/src/main/java/com/educordovi/jetpackcompisecatalogo/Scaffold.kt
@@ -2,7 +2,9 @@ package com.educordovi.jetpackcompisecatalogo
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -15,19 +17,21 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Dangerous
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
@@ -37,6 +41,7 @@ import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -136,35 +141,46 @@ fun ScaffoldWithSimpleSnackbar() {
 fun MyScaffoldTopBar() {
     val snackbarHostState = remember { SnackbarHostState() }
     val coroutineScope = rememberCoroutineScope()
-    Scaffold(
-        topBar = {
-            MyTopAppBar {
-                coroutineScope.launch { snackbarHostState.showSnackbar("Has pulsado $it") }
-            }
-        },
-        snackbarHost = {
-            SnackbarHost(
-                hostState = snackbarHostState,
-                snackbar = {
-                    Snackbar(
-                        snackbarData = it,
-                        containerColor = Color.LightGray,
-                        contentColor = Color.Blue
-                    )
-                }
-            )
-        },
-        content = { },
-        bottomBar = { MyBottomNavigation() },
-        floatingActionButton = { MyFAB() },
-        floatingActionButtonPosition = FabPosition.Center,
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
 
-        )
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet { MyDrawer() { coroutineScope.launch { drawerState.apply { close() } } } }
+        },
+    ) {
+        Scaffold(
+            topBar = {
+                MyTopAppBar(
+                    onClickIcon = {
+                        coroutineScope.launch { snackbarHostState.showSnackbar("Has pulsado $it") }
+                    },
+                    onClickDrawer = { coroutineScope.launch { drawerState.apply { if (isClosed) open() else close() } } })
+            },
+            snackbarHost = {
+                SnackbarHost(
+                    hostState = snackbarHostState,
+                    snackbar = {
+                        Snackbar(
+                            snackbarData = it,
+                            containerColor = Color.LightGray,
+                            contentColor = Color.Blue
+                        )
+                    }
+                )
+            },
+            content = { },
+            bottomBar = { MyBottomNavigation() },
+            floatingActionButton = { MyFAB() },
+            floatingActionButtonPosition = FabPosition.Center,
+
+            )
+    }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MyTopAppBar(onClickIcon: (String) -> Unit) {
+fun MyTopAppBar(onClickIcon: (String) -> Unit, onClickDrawer: () -> Unit) {
     TopAppBar(
         title = { Text(text = "Mi primera Tool Bar") },
         colors = TopAppBarDefaults.smallTopAppBarColors(
@@ -175,9 +191,9 @@ fun MyTopAppBar(onClickIcon: (String) -> Unit) {
         ),
         navigationIcon = {
             IconButton(
-                onClick = { onClickIcon("Back") }
+                onClick = { onClickDrawer() }
             ) {
-                Icon(Icons.Filled.ArrowBack, contentDescription = "back")
+                Icon(Icons.Filled.Menu, contentDescription = "menu")
             }
         },
         actions = {
@@ -236,5 +252,35 @@ fun MyFAB() {
             .offset(y = 50.dp)
     ) {
         Icon(imageVector = Icons.Filled.Add, contentDescription = "add")
+    }
+}
+
+@Composable
+fun MyDrawer(onCloseDrawer: () -> Unit) {
+    Column(Modifier.padding(8.dp)) {
+        Text(
+            text = "Primera Opcion", modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp)
+                .clickable { onCloseDrawer() }
+        )
+        Text(
+            text = "Segunda Opcion", modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp)
+                .clickable { onCloseDrawer() }
+        )
+        Text(
+            text = "Tercera Opcion", modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp)
+                .clickable { onCloseDrawer() }
+        )
+        Text(
+            text = "Cuarta Opcion", modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp)
+                .clickable { onCloseDrawer() }
+        )
     }
 }
