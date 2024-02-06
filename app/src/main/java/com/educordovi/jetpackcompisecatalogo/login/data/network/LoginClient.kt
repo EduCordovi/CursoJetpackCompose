ee565728a5b3bfc83e301ce2eb235b87197291e0
EduCordovi <educordovi@gmail.com>
2024-02-06 18:02:59 -0500

    Configurando la capa de Data

---
+++ app/src/main/java/com/educordovi/jetpackcompisecatalogo/login/data/network/LoginClient.kt
@@ -0,0 +1,10 @@
+package com.educordovi.jetpackcompisecatalogo.login.data.network
+
+import com.educordovi.jetpackcompisecatalogo.login.data.network.response.LoginResponse
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface LoginClient {
+    @GET("v3/f6d119db-ab25-4a22-8b9a-98f78dbb6aa2")
+    suspend fun doLogin(user:String, password:String):Response<LoginResponse>
+}
